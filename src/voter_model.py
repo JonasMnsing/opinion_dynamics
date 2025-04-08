78e3fe16afe85953e0d29eb8fdc83aea1dfb4c93
jonasmensing <j_mens07@uni-muenster.de>
2025-04-08 23:23:49 +0200

    base_model

--- src/voter_model.py
+++ src/voter_model.py
@@ -20,29 +20,40 @@ def random_connection_matrix(N_agents, p):
     
     return connection_matrix
 
+# def voter_interaction(opinions, connection_matrix):
+#     """One interaction: pick a random agent (i) and adopt a connected neighbor's (j) opinion."""
+
+#     N_agents    = len(opinions)     # Number of agents
+#     opinion_sum = np.sum(opinions)  # Sum of all opinions
+
+#     # Check for consensus
+#     if np.abs(opinion_sum) != N_agents:
+
+#         # Find an agent 'i' with at least one disagreeing neighbor
+#         while True:
+#             i           = np.random.randint(0, N_agents)
+#             neighbors   = np.where(connection_matrix[i]==1)[0]
+#             N_neighbors = len(neighbors)
+#             opinion_sum = np.sum(opinions[neighbors])
+
+#             if (opinion_sum - opinions[i]*N_neighbors != 0):  
+#                 break
+
+#         # Choose a disagreeing neighbor 'j' and update opinion of 'i'
+#         disagreeing_neighbors   = neighbors[opinions[neighbors] != opinions[i]]
+#         j                       = np.random.choice(disagreeing_neighbors)
+#         opinions[i]             = opinions[j]
+
+#     return opinions
+
 def voter_interaction(opinions, connection_matrix):
     """One interaction: pick a random agent (i) and adopt a connected neighbor's (j) opinion."""
 
-    N_agents    = len(opinions)     # Number of agents
-    opinion_sum = np.sum(opinions)  # Sum of all opinions
-
-    # Check for consensus
-    if np.abs(opinion_sum) != N_agents:
-
-        # Find an agent 'i' with at least one disagreeing neighbor
-        while True:
-            i           = np.random.randint(0, N_agents)
-            neighbors   = np.where(connection_matrix[i]==1)[0]
-            N_neighbors = len(neighbors)
-            opinion_sum = np.sum(opinions[neighbors])
-
-            if (opinion_sum - opinions[i]*N_neighbors != 0):  
-                break
-
-        # Choose a disagreeing neighbor 'j' and update opinion of 'i'
-        disagreeing_neighbors   = neighbors[opinions[neighbors] != opinions[i]]
-        j                       = np.random.choice(disagreeing_neighbors)
-        opinions[i]             = opinions[j]
+    N_agents    = len(opinions)                         # Number of Agents
+    i           = np.random.randint(0, N_agents)        # Sample agent i
+    neighbors   = np.where(connection_matrix[i]==1)[0]  # Find all neighbors
+    j           = np.random.choice(neighbors)           # Sample neighbor j
+    opinions[i] = opinions[j]                           # Adopt Opinion
 
     return opinions
 
@@ -71,7 +82,7 @@ if __name__ == '__main__':
 
     N_agents    = 100
     N_iter      = 10000
-    p_con       = 0.5
+    p_con       = 0.05
     avg_opinion = np.zeros(N_iter)
     int_density = np.zeros(N_iter)
     opinions    = initialize_opinions(N_agents)
@@ -82,5 +93,6 @@ if __name__ == '__main__':
         avg_opinion[i]  = average_opinion(opinions)
         int_density[i]  = interface_density(opinions, con_matrix)
 
-    np.save("avg_opinion", avg_opinion)
-    np.save("int_density", int_density)
+    np.save(f"data/avg_opinion_p={p_con}", avg_opinion)
+    np.save(f"data/int_density_p={p_con}", int_density)
+    np.save(f"data/connection_matrix_p={p_con}", con_matrix)
