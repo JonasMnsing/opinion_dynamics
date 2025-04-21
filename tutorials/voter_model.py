3e48e9dd27aed8c2c433f6c3775754d5f893a14b
jonasmensing <j_mens07@uni-muenster.de>
2025-04-21 18:05:10 +0200

    first meeting

---
+++ tutorials/voter_model.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def initialize_opinions(N_agents):
+    """Initialize agents with random opinions (-1 and 1)
+    as a 1D numpy array of length N_agents."""
+
+    return None
+
+def random_connection_matrix(N_agents, p):
+    """Generate a symmetric random adjacency matrix of size (N_agents x N_agents)
+    where each edge (i,j) exists with probability p."""
+    
+    return None
+
+def voter_interaction(opinions, connection_matrix):
+    """Sample an agent index (i) and one of its connected neighbors (j) at random.
+    Let agent i adopt the opinion of agent j.
+    """
+
+    return None
+
+# Simulation
+if __name__ == '__main__':
+
+    pass
